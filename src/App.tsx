b8825581a41468a1a2cbec7fabe51885ab4089e7
Artem <artemkholev@yandex.ru>
2023-06-17 11:36:17 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 
+import { Route, Routes } from 'react-router-dom';
+import { PageWrapper } from "./components/common/PageWrapper"
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<PageWrapper />}>
+      
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/components/common/PageWrapper/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//import footer 
+//import header
+import style from 'src/components/common/PageWrapper/PageWrapper.modules.scss';
+
+export const PageWrapper = () => (
+  <div className="page_wrapper">
+     <h1>Hello</h1>
+  </div>
+);
