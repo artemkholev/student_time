b352a039a007b41e0d01abc5fca5d6e978f438b0
Artem <artemkholev@yandex.ru>
2023-07-07 01:09:36 +0300

    create mainContainer

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from "react"
 
 import { Route, Routes } from 'react-router-dom';
 import { PageWrapper } from "./components/common/PageWrapper"
+import { MainContainer } from "./containers/MainContainer";
 
-function App() {
+const App = () => {
+
   return (
     <Routes>
       <Route path='/' element={<PageWrapper />}>
-      
+        <Route index element={<MainContainer />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/MainPage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "./mainPage.module.scss"
+
+export const MainPage = () => {
+  return (
+    <div className={style.container}>
+      <section className={style.mainSection}>
+        <p>Feel your time</p>
+      </section>
+    </div>
+  );
+}
