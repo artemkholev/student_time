15c8b90eb76f8b691388ed19d265883e32df9274
Artem <artemkholev@yandex.ru>
2023-07-08 00:19:05 +0300

    add elements in mainContent

--- student_time_front/src/components/pages/MainPage/index.tsx
+++ student_time_front/src/components/pages/MainPage/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+import mainImg from "../../../assets/photo/info.png"
 import style from "./mainPage.module.scss"
 
 export const MainPage = () => {
   return (
     <div className={style.container}>
       <section className={style.mainSection}>
-        <p>Feel your time</p>
+        <div className={style.info}>
+          <img className={style.mainSection_img} src={mainImg} alt="main img" />
+          <div className={style.topRight}>Feel your time</div>
+        </div>
       </section>
     </div>
   );
