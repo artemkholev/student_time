5c2df981671fde610612bacb38c22be03f3571e5
Artem <artemkholev@yandex.ru>
2023-07-25 00:08:34 +0300

    create new back

---
+++ student_time_back/src/test/java/site/student_time/MyFirstSite/MyFirstSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package site.student_time.MyFirstSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
